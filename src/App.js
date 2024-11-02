390f5801e2b07a2a7b285687bba932a97249f8dc
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2024-11-02 13:00:20 +0300

    homework1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Message from './components/Message';
+import './styles/Message.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Message name='World' />
+      <Message name='Marina' />
+      <Message name='Maxim' />
     </div>
   );
 }
---
+++ src/components/Message.js
@@ -0,0 +1,9 @@
+function Message({ name }) {
+  return (
+    <div className='message'>
+      <h1>Hello, {name}</h1>
+    </div>
+  );
+}
+
+export default Message;
