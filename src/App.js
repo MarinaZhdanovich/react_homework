a4a0cd569ff19d998ba0a9d373244e37c78f3aac
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2024-11-25 13:26:27 +0300

    lesson4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import TemperatureConverter from './lesson3/TemperatureConverter';
-import TodoList from './lesson3/TodoList';
+// import TemperatureConverter from './lesson3/TemperatureConverter';
+// import TodoList from './lesson3/TodoList';
+import HomePage from './lesson4/HomePage';
+import AboutPage from './lesson4/AboutPage';
 // import CommentsList from './components/CommentsList';
 // import Message from './components/Message';
-import './styles/Message.css';
+// import './styles/Message.css';
 
 function App() {
   return (
@@ -12,8 +15,14 @@ function App() {
       <Message name='Marina' />
       <Message name='Maxim' /> */}
       {/* <CommentsList /> */}
-      <TemperatureConverter />
-      <TodoList />
+      {/* <TemperatureConverter />
+      <TodoList /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
